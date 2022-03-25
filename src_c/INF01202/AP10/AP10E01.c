649d28de13c0d157fda713fb12335cfb3130a407
antonio <antonio.junior.h@gmail.com>
2022-03-25 18:32:28 -0300

    finalizando AP10E01

--- src_c/INF01202/AP10/AP10E01.c
+++ src_c/INF01202/AP10/AP10E01.c
@@ -1,95 +1,58 @@
 #include <stdio.h>
-#include <string.h>
+#define TAMANHO_MAX_VETOR 20
+#define TAMANHO_MINIMO_VETOR 1
 
-#define MAX_COMPRIMENTO 140
-#define CODIGO_lETRAS_MAIUSCULAS 1
-#define CODIGO_lETRAS_MINUSCULAS 2
-#define CODIGO_ESPACO 3
-#define CODIGO_NUMEROS 4
-#define CODIGO_CARACTERE_ESPECIAL 5
-#define CODIGO_ASC_65 65
-#define CODIGO_ASC_90 90
-#define CODIGO_ASC_97 97
-#define CODIGO_ASC_122 122
-#define CODIGO_ASC_32 32
-#define CODIGO_ASC_48 48
-#define CODIGO_ASC_57 57
-
-int determina_tipo_caractere(int codigoAsc);
+void min_e_max(double *vetor, int tamanho, double *menor, double *maior);
 
 int main()
 {
-  // dividindo as declaracoes dos ints em duas linhas para facilitar a leitura
-  int i, tamanhoFrase, quantidadeLetrasMinusculas = 0, quantidadeLetrasMaiusculas = 0;
-  int quantidadeEspacos = 0, quantidadeNumeros = 0, quantidadeCaracteresEspeciais = 0;
-  char frase[MAX_COMPRIMENTO + 2]; // + 2 para o '\n' e o '\0'
+  int i, tamanhoVetor = 0;
+  double menor, maior;
 
-  printf("Digite uma frase de ate %d de comprimento: ", MAX_COMPRIMENTO);
-  fgets(frase, MAX_COMPRIMENTO + 2, stdin);
-
-  tamanhoFrase = strlen(frase);
-
-  // elimina o '\n' lido pelo fgets()
-  frase[tamanhoFrase - 1] = '\0';
-
-  for (i = 0; i < tamanhoFrase; i++)
+  while (tamanhoVetor < TAMANHO_MINIMO_VETOR || tamanhoVetor > TAMANHO_MAX_VETOR)
   {
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &tamanhoVetor);
 
-    if (determina_tipo_caractere(frase[i]) == CODIGO_lETRAS_MAIUSCULAS)
+    if (tamanhoVetor < TAMANHO_MINIMO_VETOR || tamanhoVetor > TAMANHO_MAX_VETOR)
     {
-      quantidadeLetrasMaiusculas++;
-    }
-    else if (determina_tipo_caractere(frase[i]) == CODIGO_lETRAS_MINUSCULAS)
-    {
-      quantidadeLetrasMinusculas++;
-    }
-    else if (determina_tipo_caractere(frase[i]) == CODIGO_ESPACO)
-    {
-      quantidadeEspacos++;
-    }
-    else if (determina_tipo_caractere(frase[i]) == CODIGO_NUMEROS)
-    {
-      quantidadeNumeros++;
-    }
-    else if (determina_tipo_caractere(frase[i]) == CODIGO_CARACTERE_ESPECIAL)
-    {
-      quantidadeCaracteresEspeciais++;
+      printf("Valor incorreto! O valor digitado deve ser entre 1 e 20\n\n");
     }
   }
 
-  printf("\nEspacos: %d", quantidadeEspacos);
-  printf("\nLetras minusculas: %d", quantidadeLetrasMinusculas);
-  printf("\nLetras maiusculas: %d", quantidadeLetrasMaiusculas);
-  printf("\nDigitos numericos: %d", quantidadeNumeros);
-  printf("\nCaracteres especiais: %d\n\n", quantidadeCaracteresEspeciais - 1); // subtraindo 1 para desconsiderar o 0 colocado por default na string
+  // declarando vetor fora do inicio do programa, pois nao sabemos o tamanho dele até o usuario digitar
+  double vetor[tamanhoVetor];
+
+  for (i = 0; i < tamanhoVetor; i++)
+  {
+    printf("Digite o valor #%d do vetor: ", i + 1);
+    scanf("%lf", &vetor[i]);
+  }
+
+  min_e_max(vetor, tamanhoVetor, &menor, &maior);
+
+  printf("Valor minimo: %.1lf", menor);
+  printf("\nValor maximo: %.1lf\n\n", maior);
 
   return 0;
 }
 
-int determina_tipo_caractere(int codigoAsc)
+void min_e_max(double *vetor, int tamanho, double *menor, double *maior)
 {
-  int codigoTipoCaractere;
+  int i;
+  *menor = vetor[0];
+  *maior = vetor[0];
 
-  if (codigoAsc >= CODIGO_ASC_65 && codigoAsc <= CODIGO_ASC_90)
+  for (i = 0; i < tamanho; i++)
   {
-    codigoTipoCaractere = CODIGO_lETRAS_MAIUSCULAS;
-  }
-  else if (codigoAsc >= CODIGO_ASC_97 && codigoAsc <= CODIGO_ASC_122)
-  {
-    codigoTipoCaractere = CODIGO_lETRAS_MINUSCULAS;
-  }
-  else if (codigoAsc == CODIGO_ASC_32)
-  {
-    codigoTipoCaractere = CODIGO_ESPACO;
-  }
-  else if (codigoAsc >= CODIGO_ASC_48 && codigoAsc <= CODIGO_ASC_57)
-  {
-    codigoTipoCaractere = CODIGO_NUMEROS;
-  }
-  else
-  {
-    codigoTipoCaractere = CODIGO_CARACTERE_ESPECIAL;
-  }
+    if (vetor[i] > *maior)
+    {
+      *maior = vetor[i];
+    }
 
-  return codigoTipoCaractere;
+    if (vetor[i] < *menor)
+    {
+      *menor = vetor[i];
+    }
+  }
 }
